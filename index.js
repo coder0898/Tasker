9cd6f4ccfba46881f9418faa49df5b8fa9bc7b7d
Pratik Bakshi <bakshi.pratikcool@gmail.com>
2025-01-13 11:19:06 +0530

    compeleting add task modal and refactoring css

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+// tabs
 const tabsBtn = document.querySelectorAll(".tab-btns .tabs-btn");
 const tabsContent = document.querySelectorAll(".container section");
+
+// modal
+const modalContainer = document.getElementById("modal-container");
+const addTaskBtn = document.getElementById("addTask-btn");
+const closedBtn = document.getElementById("close-btn");
+
+// modal form
+const taskName = document.getElementById("task-name");
+const descriptionText = document.getElementById("description-text");
+const taskDate = document.getElementById("task-date");
+const taskAssign = document.getElementById("task-assign");
+const resetBtn = document.getElementById("reset-btn");
+const submitBtn = document.getElementById("submit-btn");
 
 // switching tabs functionality
 tabsBtn.forEach((tab, index) => {
@@ -13,4 +27,54 @@ tabsBtn.forEach((tab, index) => {
     tabsContent[index].classList.add("active");
     tabsBtn[index].classList.add("active-btn");
   });
+});
+
+// model functionality
+
+addTaskBtn.addEventListener("click", () => {
+  modalContainer.style.display = "table";
+});
+
+// 2.When the user clicks on <span> (x), close the modal
+closedBtn.addEventListener("click", () => {
+  modalContainer.style.display = "none";
+});
+
+// 3.When the user clicks anywhere outside of the modal, close it
+window.addEventListener("click", (event) => {
+  if (event.target == modalContainer) {
+    modalContainer.style.display = "none";
+  }
+});
+
+// modal form functionality
+let formData;
+function FormValidation() {
+  let taskTitle = taskName.value,
+    taskDesc = descriptionText.value,
+    dateOfTask = taskDate.value,
+    assignTo = taskAssign.value;
+
+  if (taskTitle == "" || taskDesc == "" || !dateOfTask || assignTo == "") {
+    alert("Please enter valid details...");
+  } else {
+    formData = {
+      title: taskTitle,
+      desc: taskDesc,
+      endDate: dateOfTask,
+      assign: assignTo,
+    };
+    console.log(formData);
+  }
+}
+
+submitBtn.addEventListener("click", () => {
+  FormValidation();
+});
+
+resetBtn.addEventListener("click", () => {
+  taskName.value = "";
+  descriptionText.value = "";
+  taskDate.value = "";
+  taskAssign.value = "";
 });
